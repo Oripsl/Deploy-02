1cc6268171db860a97c500823dbee9e90dd755ac
Basile Bartolomeo <basilebartolomeo01@gmail.com>
2024-05-12 14:22:02 +0200

    first commit

---
+++ src/main/java/com/orcone/sasso/SassoApplication.java
@@ -0,0 +1,13 @@
+package com.orcone.sasso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SassoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SassoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orcone/sasso/SassoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orcone.sasso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SassoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
